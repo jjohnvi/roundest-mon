4b7e08c84c5fc832ef1fa718d3fadbc30b9146a8
John Jeong <jjohnvi@Johns-MBP.lan>
2022-08-09 12:38:30 -0500

    experiencing hydration error and text content error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { trpc } from '@/utils/trpc'
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
 
 
 const Home: NextPage = () => {
+
+  const [first, second] = getOptionsForVote();
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
       <div className='p-2'/>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
     <div className='p-8'>Vs</div>
-    <div className='w-16 h-16 bg-red-200'></div>
+    <div className='w-16 h-16 bg-red-800'>{second}</div>
     </div>
     </div>
   )
