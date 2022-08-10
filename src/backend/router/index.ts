785f094f9556a6d6e088bb81e9d25f000672e360
John Jeong <jjohnvi@Johns-MBP.lan>
2022-08-10 14:55:44 -0500

    send back way less data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -11,7 +11,8 @@ export const appRouter = trpc
       const api = new PokemonClient();
 
       const pokemon = await api.getPokemonById(input.id)
-      return pokemon;
+      return {name: pokemon.name, sprites:pokemon.sprites};
+      
     }
   });
 
